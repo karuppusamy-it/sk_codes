537e482f808fa0b82549eb9ca492ed547b83eddd
KARUPPUSAMY S <karuppusamy.it23@bitsathy.ac.in>
2025-02-20 10:00:55 +0530

    Time: 587 ms (54.93%), Space: 20.6 MB (84.61%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort()
+        nums.sort()  
+        n = len(nums)
 
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
-            j = i + 1
-            k = len(nums) - 1
+        # 1st element 
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]: continue  # Same as previous so can skip. 
 
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
+            # Looking for 2nd and 3rd elements
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                # Success
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
 
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
-                else:
-                    res.append([nums[i], nums[j], nums[k]])
-                    j += 1
+                    # Search for last repeat left pointer. 
+                    while left < right and nums[left] == nums[left + 1]: 
+                        left += 1  
+                    # Search for last repeat right pointer
+                    while left < right and nums[right] == nums[right - 1]: 
+                        right -= 1  
 
-                    while nums[j] == nums[j-1] and j < k:
-                        j += 1
-        
+                    # Shrink window
+                    left += 1
+                    right -= 1
+
+                elif total < 0: left += 1 # Need more
+                else: right -= 1 # Need less
+
         return res
